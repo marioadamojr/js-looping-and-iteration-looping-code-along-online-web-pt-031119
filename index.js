ee07d2e24e21c8c1c1e7c3d25909edbd56829700
Mario Adamo Jr <marioadamojr@gmail.com>
2019-10-02 22:24:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solutions in this file
 function writeCards(names, occasion) {
-  let returnMessages = []
+  let returnMessages = [];
   for (let i = 0; i < cards.length; i++) {
     returnMessages.push(`Thank you, ${names[i]}, for the wonderful ${occasion} gift!`)
   }
