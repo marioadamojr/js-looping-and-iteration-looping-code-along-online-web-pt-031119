4787ec3a3d92ec53629b905c5b9c00af94f1bb01
Mario Adamo Jr <marioadamojr@gmail.com>
2019-10-02 22:19:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Code your solutions in this file
+function writeCards(cards) {
+  for (let i = 0; i < cards.length; i++) {
+    console.log(`Thank you, ${cards[i]}, for the wonderful birthdat gift!`)
+  }
+  return cards;
+}
+
