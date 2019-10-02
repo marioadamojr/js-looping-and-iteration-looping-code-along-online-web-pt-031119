57a6d6e11af25e6fea96504c884a6c0d2f585718
Mario Adamo Jr <marioadamojr@gmail.com>
2019-10-02 22:37:06 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+function writeCards(names, occasion) {
+  let returnMessages = [];
+  for (let i = 0; i < names.length; i++) {
+    returnMessages.push(`Thank you, ${names[i]}, for the wonderful ${occasion} gift!`)
+  }
+  return returnMessages;
+}
+
+function countdown(n){
+  while (n >= 0){
+    console.log(n--);
+  }
+}
